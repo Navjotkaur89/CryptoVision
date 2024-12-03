3941041a42a64e6f5acf37905f6a85ad3655f9f3
Navjotkaur89 <kaurnavjot8968@gmail.com>
2024-12-03 21:26:20 +0530

    new changes

--- newsapi.py
+++ newsapi.py
@@ -1,17 +1,18 @@
 import requests
 
-url = "https://cryptocurrency-news2.p.rapidapi.com/v1/coindesk"
+url = "https://cryptocurrency-news2.p.rapidapi.com/v1/cryptodaily"
 
 def cryptonews():
     headers = {
 	"x-rapidapi-key": "312617ff61msh2751704ca7e6b9cp13af57jsnbf25308174d7",
 	"x-rapidapi-host": "cryptocurrency-news2.p.rapidapi.com"
-    }
+}
+
 
 
 
 
     response = requests.get(url, headers=headers)
-    # print(response.json())
+    #print(response.json())
     return response.json()
 cryptonews()
